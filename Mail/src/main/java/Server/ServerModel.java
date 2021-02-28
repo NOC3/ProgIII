d906476bbcb6a10c0acd0279ec607ee17a700a0b
fedeztk <federicoserranexus@gmail.com>
2021-02-28 19:26:20 +0100

    bozza server

--- Mail/src/main/java/Server/ServerModel.java
+++ Mail/src/main/java/Server/ServerModel.java
@@ -1,19 +1,26 @@
 package Server;
 
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
 import java.io.File;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ServerModel {
     Set<String> usersMail; //contains all "registered"/valid user's mail
+    Server srv;
 
     public ServerModel() {
         usersMail = buildUsersMail();
+        srv = new Server();
     }
 
+    //forse meglio set<File> per accessi futuri, es eliminazione/nuova mail
     public Set<String> buildUsersMail() {
-
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
         URL url = loader.getResource("./mails/");
         assert url != null;
@@ -32,9 +39,40 @@ public class ServerModel {
         return allUsers;
     }
 
-    //innestare una classe che estenda Task (Server, il pool)
-    // e una che implementi i singoli Runnable (switch su Message.codice)
-    class Server {
-        private int c;
+
+    /*
+    * bozza per il 01/03
+    * */
+
+    //classi interne al model di comunicazione
+
+    //forse deve essere un thread autonomo
+    class Server { //la pool
+        ServerSocket serverSocket;
+        private static final int THREADNUM = 10;
+
+        public Server(){
+            try {
+                serverSocket = new ServerSocket();
+            } catch (Exception e){
+                System.out.println(e.getLocalizedMessage());
+            }
+        }
+
+
+        class Request implements Runnable{ //le singole request
+            Socket clientSocket;
+            ServerModel model;
+
+            public Request(){
+                clientSocket = new Socket();
+                model = ServerModel.this;
+            }
+
+            @Override
+            public void run() {
+
+            }
+        }
     }
 }
