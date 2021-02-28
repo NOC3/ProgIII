516ee51d8055e604f089136da7d4089e4ee1fd5f
fedeztk <federicoserranexus@gmail.com>
2021-02-28 13:10:55 +0100

    fixed file retrieving

--- Mail/src/main/java/Server/ServerModel.java
+++ Mail/src/main/java/Server/ServerModel.java
@@ -1,17 +1,12 @@
 package Server;
 
-import Common.*;
-
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ServerModel {
-    Set<String> usersMail;
+    Set<String> usersMail; //contains all "registered"/valid user's mail
 
     public ServerModel() {
         usersMail = buildUsersMail();
@@ -19,33 +14,19 @@ public class ServerModel {
 
     public Set<String> buildUsersMail() {
 
-        File folder = new File(String.valueOf((getClass().getResource("/mails/ciao.json"))));
-        //      File folder = new File(System.getProperty("../../resources/mails"));
-
-        System.out.println("Cartella : " + folder.getName());
-        System.out.println("Cartella : " + folder.isDirectory());
-        System.out.println("Cartella : " + folder.isFile());
-        System.out.println("Cartella : " + folder.getAbsolutePath());
-        System.out.println("Cartella : " + folder.canRead());
-        System.out.println("Cartella : " + folder.toString());
-        System.out.println("Contenuto : " + Arrays.toString(folder.list()));
-        System.out.println();
-        try {
-            System.out.println("Walk "+Files.walk(Path.of(String.valueOf((getClass().getResource("/mails/"))))));
-        }catch(Exception e){
-            System.out.println(e);
-        }
-        System.out.println(folder.exists());
-
-        File[] listOfFiles = folder.listFiles();
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        URL url = loader.getResource("./mails/");
+        assert url != null;
+        String path = url.getPath();
+        File [] listOfFiles = new File(path).listFiles();
 
         Set<String> allUsers = new HashSet<>();
-
         assert listOfFiles != null;
 
         for (File f : listOfFiles)
-            allUsers.add(f.getName());
+            allUsers.add(f.getName().substring(0, f.getName().length()-5)); //removing .json
 
+        //debug
         System.out.println(allUsers);
 
         return allUsers;
